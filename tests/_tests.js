5f0752a9d8e3480134d71699b297dad8cd47f7d8
Svein Olav Risdal <svein.olav.risdal@gmail.com>
2014-09-22 15:37:20 +0200

    Modified test suite

--- tests/_tests.js
+++ tests/_tests.js
@@ -2,11 +2,20 @@ var tape = require( './_tape.mock.js' )
 	, test = tape.test
 
 	, backwards   = require( '../src/backwards.js' )
+
 	, isArguments = require( './isArguments.test.js' )
 	, isArray     = require( './isArray.test.js' )
+	, isBoolean   = require( './isBoolean.test.js' )
+	, isFunction  = require( './isFunction.test.js' )
+	, isNull      = require( './isNull.test.js' )
+	, isNumber    = require( './isNumber.test.js' )
 ;
 
 isArguments( test, backwards );
 isArray( test, backwards );
+isBoolean( test, backwards );
+isFunction( test, backwards );
+isNull( test, backwards );
+isNumber( test, backwards );
 
-this.global_test_results = tape.getResults();
+window.global_test_results = tape.getResults();
