c2e4ef6234f95016724d23d2c6a87739b496aa6d
Svein Olav Risdal <svein.olav.risdal@gmail.com>
2014-09-29 14:03:23 +0200

    Modified test suite

--- tests/_tests.js
+++ tests/_tests.js
@@ -14,7 +14,7 @@ var tape        = require( './_tape.mock.js' )
 	, isNull      = require( './isNull.test.js' )( test, backwards )
 	, isNumber    = require( './isNumber.test.js' )( test, backwards )
 	, isObject    = require( './isObject.test.js' )( test, backwards )
-	, isString    = require( './isString.test.js' )( test, backwards )
+	// , isString    = require( './isString.test.js' )( test, backwards )
 	// , isUndefined = require( './isUndefined.test.js' )( test, backwards )
 
 	// , compose     = require( './compose.test.js' )( test, backwards )
