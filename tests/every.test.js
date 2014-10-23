17b6192db0e44b988517d9bc3e80cf544f83b68c
Svein Olav Risdal <svein.olav.risdal@gmail.com>
2014-10-23 22:06:05 +0200

    Added unit test for backwards.every

---
+++ tests/every.test.js
@@ -0,0 +1,30 @@
+module.exports = function (test, backwards) {
+	var every  = backwards.every
+		, txt    = 'backwards.every should '
+		, passed = every( isBigEnough )( [12, 54, 18, 130, 44] )
+		, failed = every( isBigEnough )( [12, 5, 8, 130, 44] );
+
+	function isBigEnough (x, i, arr) {
+		return x > 10;
+	}
+	
+	test(txt + 'be a function', function (t) {
+		t.equal(typeof every, 'function');
+		t.end();
+	});
+
+	test('return false if not every number is big enough', function (t) {
+		t.equal( failed, false );
+		t.end();
+	});
+
+	test('return true if every number is big enough', function (t) {
+		t.equal( passed, true );
+		t.end();
+	});
+
+	test('return true if given an empty Array', function (t) {
+		t.equal( every(isBigEnough, []), true );
+		t.end();
+	});
+};
